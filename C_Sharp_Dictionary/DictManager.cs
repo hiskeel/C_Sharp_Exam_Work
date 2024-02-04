2c8ad76defb061ab2eef02960f23f328350ca4a3
hiskeel <borovetsmykola27@gmail.com>
2024-02-04 16:38:17 +0200

    Initialize

---
+++ C_Sharp_Dictionary/DictManager.cs
@@ -0,0 +1,64 @@
+﻿namespace C_Sharp_Dictionary_
+{
+    partial class DictManager
+    {
+        Dictionary<string, Dictionary<string, List<string>>> dictionaries;
+        public DictManager()
+        {
+            dictionaries = new Dictionary<string, Dictionary<string, List<string>>>();
+        }
+
+        public string ChooseDictionary()
+        {
+            string name;
+            int choice;
+            int count = 1;
+            foreach (string item in dictionaries.Keys)
+            {
+                Console.WriteLine($"{count}. {item}");
+                count++;
+            }
+            choice = int.Parse(Console.ReadLine());
+            if (dictionaries.Count > count || dictionaries.Count < 1)
+            {
+                Console.WriteLine("Wrong choice!");
+                return null;
+            }
+
+            count = 1;
+            foreach (string item in dictionaries.Keys)
+            {
+                if (count == choice) return item;
+                else count++;
+            }
+            return null;
+        }
+        public void ShowDictionary(string dictName)
+        {
+            foreach(var item in dictionaries[dictName])
+            {
+                Console.Write($"{item.Key} - ");
+                foreach(string i in item.Value)
+                {
+                    Console.Write($"{i}, ");
+                    Console.WriteLine();
+                }
+            }
+        }
+        public void CreateDictionary()
+        {
+            Console.WriteLine("Введіть назву словника:");
+            string dictionaryName = Console.ReadLine();
+
+            if (!dictionaries.ContainsKey(dictionaryName))
+            {
+                dictionaries[dictionaryName] = new Dictionary<string, List<string>>();
+                Console.WriteLine($"Створено словник '{dictionaryName}'.");
+            }
+            else
+            {
+                Console.WriteLine("Словник з такою назвою вже існує.");
+            }
+        }
+    }
+}
---
+++ C_Sharp_Dictionary/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace C_Sharp_Dictionary_
+{
+
+    internal class Program
+    {
+        class Menu
+        {
+            public void MainMenu()
+            {
+                Console.WriteLine("Меню:");
+                Console.WriteLine("1. Створити словник");
+                Console.WriteLine("2. Додати слово і його переклад");
+                Console.WriteLine("3. Зміна слова або перекладу");
+                Console.WriteLine("4. Видалення слова або перекладу");
+                Console.WriteLine("5. Шукати переклад слова");
+                Console.WriteLine("6. Експорт слова та перекладу до файлу");
+                Console.WriteLine("0. Вихід");
+            }
+            public void MenuAdd()
+            {
+                Console.Clear();
+
+            }
+            public void MenuDel()
+            {
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            DictManager dict = new DictManager();
+            
+
+        }
+        
+    }
+}

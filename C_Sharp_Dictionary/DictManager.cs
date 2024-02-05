86591051132abc86b40de3ee68f6d0c0d70ae516
hiskeel <borovetsmykola27@gmail.com>
2024-02-05 03:26:40 +0200

    v 0.0.1

--- C_Sharp_Dictionary/DictManager.cs
+++ C_Sharp_Dictionary/DictManager.cs
@@ -1,36 +1,215 @@
-﻿namespace C_Sharp_Dictionary_
+﻿using System.Xml;
+using System.Text.Json;
+using Newtonsoft.Json;
+
+namespace C_Sharp_Dictionary_
 {
     partial class DictManager
     {
         Dictionary<string, Dictionary<string, List<string>>> dictionaries;
+        public List<string> DictionaryNamesList
+        {
+            get { return new List<string>(dictionaries.Keys); }
+        }
+       
         public DictManager()
         {
             dictionaries = new Dictionary<string, Dictionary<string, List<string>>>();
         }
 
+        public void EditWordData(string dictName) {
+            bool exit = false;
+            
+            Console.WriteLine("Введіть слово яке хочете змінити:");
+            string wordToEdit = Console.ReadLine();
+            if (dictionaries[dictName].ContainsKey(wordToEdit)) {
+                Console.WriteLine("1. Змінити саме слово\n2. Змінити переклад");
+                int choice = int.Parse(Console.ReadLine()); 
+                if (choice == 1) {
+                    Console.WriteLine("Введіть нове слово:");
+                    dictionaries[dictName][Console.ReadLine()] = dictionaries[dictName][wordToEdit];
+                    dictionaries[dictName].Remove(wordToEdit);
+                    Console.WriteLine($"Слово \"{wordToEdit} \" було змінене.");
+                }
+                else if(choice == 2)
+                {
+                    Console.WriteLine("1. Залишити переклад який був і додати новий\n2. Повністю змінити переклад");
+                    choice = int.Parse(Console.ReadLine());
+                    if (choice == 1)
+                    {
+                        List<string> translations = dictionaries[dictName][wordToEdit];
+                        while (!exit)
+                        {
+                            Console.Write("Введіть переклад вашого слова(Щоб принити ввід перекладу введіть\" 0 \"): ");
+                            string wordTranslation = Console.ReadLine();
+                            if (wordTranslation == "0")
+                            {
+                                exit = true;
+                            }
+                            else
+                            {
+                                translations.Add(wordTranslation);
+                            }
+                        }
+                        dictionaries[dictName][wordToEdit] = translations;
+                    }
+                    else if (choice == 2)
+                    {
+                        List<string> translations = new List<string>();
+                        while (!exit)
+                        {
+                            Console.Write("Введіть переклад вашого слова(Щоб принити ввід перекладу введіть\" 0 \"): ");
+                            string wordTranslation = Console.ReadLine();
+                            if (wordTranslation == "0")
+                            {
+                                exit = true;
+                            }
+                            else
+                            {
+                                translations.Add(wordTranslation);
+                            }
+                        }
+                        dictionaries[dictName][wordToEdit] = translations;
+                    }
+
+                    else { 
+                        Console.Clear();
+                        Console.WriteLine("Не вірно вказаний вибір!");
+                        Console.WriteLine(); }
+                }
+            }
+            else Console.WriteLine($"Немає слова {wordToEdit} у словнику : {dictName}");
+           
+        }
+        public void SearchForWord(string dictName)
+        {
+            Console.WriteLine("Введіть слово яке ви хочете знайти:");
+            string word = Console.ReadLine() ;
+            if (dictionaries[dictName].ContainsKey(word)) {
+                Console.WriteLine($"{word} :");
+                ShowTranslations(dictName, word);
+                Console.WriteLine("----------------------");
+            }
+            else Console.WriteLine($"У словнику \"{dictName}\" немає слова {word}.");
+        }
+
+        void ShowTranslations(string dictName,string word)
+        {
+          
+            int count = 1;
+            foreach(string item in dictionaries[dictName][word])
+            {
+                Console.WriteLine($"{count}. {item};");
+                count++;
+            }
+          
+        }
+        public void DeleteWordOrTranslation(string dictName)
+        {
+            Console.WriteLine("1. Видалити слово повністю\n2. Видалити переклад слова");
+            int choice = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть слово яке ви хочете видалити:");
+            string wordToDel = Console.ReadLine();
+            if (choice == 1) {
+                
+               
+                if (dictionaries[dictName].ContainsKey(wordToDel)) {
+                    dictionaries[dictName].Remove(wordToDel);
+                    Console.WriteLine($"Слово {wordToDel} було видалено зі словника.");
+                }
+                else Console.WriteLine($"У даному словнику немає слова \"{wordToDel}\".");
+            }
+            else if(choice == 2)
+            {
+                if (dictionaries[dictName].ContainsKey(wordToDel))
+                {
+                    if (dictionaries[dictName][wordToDel].Count == 1) Console.WriteLine("Ви не можете видалити останній переклад слова.");
+                    else
+                    {
+                        Console.WriteLine($"Оберіть переклад слова який ви хочете видалити:");
+                        ShowTranslations(dictName, wordToDel);
+                        try
+                        {
+                            choice = int.Parse(Console.ReadLine());
+                            if (choice < 1 || choice > dictionaries[dictName][wordToDel].Count) Console.WriteLine($"Немає слів з індексом {choice}.");
+                            else
+                            {
+                                dictionaries[dictName][wordToDel].Remove(dictionaries[dictName][wordToDel][choice - 1]);
+                            }
+
+
+                        }
+                        catch (Exception ex) { Console.WriteLine(ex.Message); }
+                    }
+                }
+                else Console.WriteLine($"У даному словнику немає слова \"{wordToDel}\".");
+            }
+           
+          
+         }
+        public void AddWordToDictionary(string dictName)
+        {
+
+            bool exit = false;
+            Console.WriteLine("Оберіть словник до якого ви хочете додати нове слово:");
+            
+            Console.WriteLine("Введіть слово яке ви хочете додати:");
+            string word = Console.ReadLine();
+            if (!dictionaries[dictName].ContainsKey(word))
+            {
+       
+                List<string> translations = new List<string>();
+
+                while (!exit)
+                {
+                    Console.Write("Введіть переклад вашого слова(Щоб принити ввід перекладу введіть\" 0 \"): ");
+                    string wordTranslation = Console.ReadLine();
+                    if (wordTranslation == "0")
+                    {
+                        exit = true;
+                    }
+                    else
+                    {
+                        translations.Add(wordTranslation);
+                    }
+                }
+                dictionaries[dictName].Add(word, translations);
+
+                Console.WriteLine($"Слово '{word}' Та я його переклад додано до словника: '{dictName}'.");
+            }
+            else
+            {
+                Console.WriteLine($"Слово '{word}' Уже існує у словнику: '{dictName}'.");
+            }
+        }
         public string ChooseDictionary()
         {
             string name;
             int choice;
             int count = 1;
-            foreach (string item in dictionaries.Keys)
+            if (dictionaries.Keys.Count!=0)
             {
-                Console.WriteLine($"{count}. {item}");
-                count++;
-            }
-            choice = int.Parse(Console.ReadLine());
-            if (dictionaries.Count > count || dictionaries.Count < 1)
-            {
-                Console.WriteLine("Wrong choice!");
-                return null;
-            }
+                foreach (string item in dictionaries.Keys)
+                {
+                    Console.WriteLine($"{count}. {item}");
+                    count++;
+                }
+                choice = int.Parse(Console.ReadLine());
+                if (dictionaries.Count > count || dictionaries.Count < 1)
+                {
+                    Console.WriteLine("Wrong choice!");
+                    return null;
+                }
 
-            count = 1;
-            foreach (string item in dictionaries.Keys)
-            {
-                if (count == choice) return item;
-                else count++;
+                count = 1;
+                foreach (string item in dictionaries.Keys)
+                {
+                    if (count == choice) return item;
+                    else count++;
+                }
             }
+            else Console.WriteLine("У вас немає словників з якими ви можете працювати!");
+
             return null;
         }
         public void ShowDictionary(string dictName)
@@ -41,9 +220,11 @@
                 foreach(string i in item.Value)
                 {
                     Console.Write($"{i}, ");
-                    Console.WriteLine();
+                    
                 }
+                Console.WriteLine();
             }
+            
         }
         public void CreateDictionary()
         {
@@ -58,6 +239,112 @@
             else
             {
                 Console.WriteLine("Словник з такою назвою вже існує.");
+            }
+        }
+        public void SaveDictionariesListToFile()
+        {
+            try
+            {
+                List<string> dictionariesList = new List<string>();
+                foreach (var item in dictionaries.Keys)
+                {
+                    dictionariesList.Add(item);
+                }
+                string json = JsonConvert.SerializeObject(dictionariesList, Newtonsoft.Json.Formatting.Indented);
+                File.WriteAllText("Dictionaries.json", json);
+                Console.WriteLine($"Список словників збережено до файлу 'Dictionaries.json'.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Помилка збереження словників до файлу: {ex.Message}");
+            }
+        }
+        public List<string> LoadDictionariesListFromFile()
+        {
+            try
+            {
+                if (File.Exists("Dictionaries.json"))
+                {
+                    string json = File.ReadAllText("Dictionaries.json");
+                    return JsonConvert.DeserializeObject<List<string>>(json);
+                }
+                else
+                {
+                    Console.WriteLine($"Файлу \"Dictionaries.json\" не існує.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Помилка загрузки словників із файлу: {ex.Message}");
+            }
+
+            return null;
+        }
+        public void SaveDictionaryToFile(string dictName, string fileName)
+        {
+            if (dictionaries.ContainsKey(dictName))
+            {
+                try
+                {
+                    string json = JsonConvert.SerializeObject(dictionaries[dictName], Newtonsoft.Json.Formatting.Indented);
+                    File.WriteAllText(fileName, json);
+                    Console.WriteLine($"Словник '{dictName}' було збережено до файлу '{dictName}.txt'.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Помилка зберігання словника до файлу: {ex.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Словника з ім'ям '{dictName}' не існує.");
+            }
+        }
+        public void LoadDictionaryFromFile(string fileName)
+        {
+            try
+            {
+                if (File.Exists(fileName))
+                {
+                    string json = File.ReadAllText(fileName);
+                    
+                    dictionaries.Add(fileName.Substring(0, fileName.Length - 5), JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(json));
+                }
+                else
+                {
+                    Console.WriteLine($"Файла з назвою '{fileName}' не існує.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Помилка загрузки словника із файлу: {ex.Message}");
+            }
+
+           
+        }
+        public void ExportWordToFile(string dictName, string word)
+        {
+            if (dictionaries[dictName].ContainsKey(word))
+            {
+                try
+                {
+                    Dictionary<string, List<string>> exportData = new Dictionary<string, List<string>>
+                {
+                    { word, dictionaries[dictName][word] }
+                };
+
+                    string json = JsonConvert.SerializeObject(exportData, Newtonsoft.Json.Formatting.Indented);
+                    File.WriteAllText($"{word}.json", json);
+                    Console.WriteLine($"Слово '{word}' з словника '{dictName}' було експортовано до '{word}.json'.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Помилка експортування слова: {ex.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Слова '{word}' не існує у словнику: '{dictName}'.");
             }
         }
     }
--- C_Sharp_Dictionary/Program.cs
+++ C_Sharp_Dictionary/Program.cs
@@ -9,35 +9,151 @@ namespace C_Sharp_Dictionary_
     {
         class Menu
         {
-            public void MainMenu()
+
+            public void MenuStart()
             {
                 Console.WriteLine("Меню:");
-                Console.WriteLine("1. Створити словник");
-                Console.WriteLine("2. Додати слово і його переклад");
-                Console.WriteLine("3. Зміна слова або перекладу");
-                Console.WriteLine("4. Видалення слова або перекладу");
-                Console.WriteLine("5. Шукати переклад слова");
-                Console.WriteLine("6. Експорт слова та перекладу до файлу");
+                Console.WriteLine("" +
+                    "1. Обрати словник з яким ви хочете працювати:\n" +
+                    "2. Створити Новий словник");
                 Console.WriteLine("0. Вихід");
             }
-            public void MenuAdd()
+            public void MenuMain(string dictName)
             {
-                Console.Clear();
+                Console.WriteLine($"Ви працюєте із словником: \"{dictName}\"!\n\n" + $"Оберіть дію яку ви хочете виконати:\n" + 
+                    $"1. Додати нове слово\n" + 
+                    $"2. Замінити слово або його переклад\n" + 
+                    $"3. Шукати переклад слова \n" +
+                    $"4. Видалити слово або його переклад\n" +
+                    $"5. Показати весь словник\n" +
+                    $"6. Експортувати слово в окремий файл\n" +
+                    $"9. Обрати інший словник\n" +
+                    $"0. Вийти з програми");
 
             }
-            public void MenuDel()
+            public void Clear()
             {
-
+                Console.WriteLine("\n\nНатисніть будь-яку кнопку щоб продовжити...\n");
+                Console.ReadKey();
+                Console.Clear();
             }
         }
 
         static void Main(string[] args)
         {
+            
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             DictManager dict = new DictManager();
+            Menu menu= new Menu();
+            bool exit = false;
+            bool back = false;
+            try
+            {
+                List<string> dicts = dict.LoadDictionariesListFromFile();
+                foreach (var item in dicts)
+                {
+                    dict.LoadDictionaryFromFile($"{item}.json");
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
             
 
-        }
-        
+
+            while (!exit)
+            {
+                
+                    menu.MenuStart();
+                    string choice = Console.ReadLine();
+                    switch (choice)
+                    {
+                        case "1":
+                        Console.Clear();
+                        
+                            string dictName = dict.ChooseDictionary();
+
+                        if (dictName != null)
+                        {
+                            back = false;
+                            while (!back)
+                            {
+                                Console.Clear();
+                                menu.MenuMain(dictName);
+                                choice = Console.ReadLine();
+                                switch (choice)
+                                {
+                                    case "1":
+                                        
+                                        dict.AddWordToDictionary(dictName);
+                                        menu.Clear();
+                                        break;
+                                    case "2":
+                                        
+                                        dict.EditWordData(dictName);
+                                        menu.Clear();
+                                        break;
+
+                                    case "3":
+                                        
+                                        dict.SearchForWord(dictName);
+                                        menu.Clear();
+                                        break;
+                                    case "4":
+                                        
+                                        dict.DeleteWordOrTranslation(dictName);
+                                        menu.Clear();
+                                        break;
+
+                                    case "5":
+                                        dict.ShowDictionary(dictName);
+                                        menu.Clear();
+                                        break;
+                                    case "6":
+                                        
+                                        Console.WriteLine("Введіть слово яке ви хочете Експортувати в окремий файл");
+                                        string word = Console.ReadLine();
+                                        dict.ExportWordToFile(dictName, word);
+                                        menu.Clear();
+                                        break;
+                                    case "9":
+                                        Console.Clear();
+                                        back = true;
+                                        break;
+                                    case "0":
+                                        back = true;
+                                        exit = true;
+                                        Console.WriteLine("Всі зміни буду збережено.\nГарного вам дня!");
+                                        break;
+                                    default:
+                                        Console.WriteLine("Не коректно вказаний пункт меню!");
+                                        break;
+                                }
+
+                            }
+                        }
+
+                        
+                            break;
+                        case "2":
+                        
+
+                        dict.CreateDictionary();
+                        menu.Clear();
+                        break;
+
+                        case "0": Console.WriteLine("Всі зміни буду збережено.\nГарного дня"); Console.ReadKey(); break;
+
+                        default:
+                            Console.WriteLine("Не коректно вказаний пункт меню!");
+                            Console.ReadKey();
+                            break;
+
+                    }                           
+            }
+            dict.SaveDictionariesListToFile();
+            foreach(var item in dict.DictionaryNamesList) 
+            {
+                dict.SaveDictionaryToFile(item, $"{item}.json");
+            }
+        }        
     }
 }

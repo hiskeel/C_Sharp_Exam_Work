b5be7fdfb8b2122c56d3559b4dc3d593d92c7ce6
hiskeel <borovetsmykola27@gmail.com>
2024-02-05 03:29:00 +0200

    Code view fix

---
+++ C_Sharp_Dictionary/Menu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Dictionary_
+{
+    partial class Menu
+    {
+
+        public void MenuStart()
+        {
+            Console.WriteLine("Меню:");
+            Console.WriteLine("" +
+                "1. Обрати словник з яким ви хочете працювати:\n" +
+                "2. Створити Новий словник");
+            Console.WriteLine("0. Вихід");
+        }
+        public void MenuMain(string dictName)
+        {
+            Console.WriteLine($"Ви працюєте із словником: \"{dictName}\"!\n\n" + $"Оберіть дію яку ви хочете виконати:\n" +
+                $"1. Додати нове слово\n" +
+                $"2. Замінити слово або його переклад\n" +
+                $"3. Шукати переклад слова \n" +
+                $"4. Видалити слово або його переклад\n" +
+                $"5. Показати весь словник\n" +
+                $"6. Експортувати слово в окремий файл\n" +
+                $"9. Обрати інший словник\n" +
+                $"0. Вийти з програми");
+
+        }
+        public void Clear()
+        {
+            Console.WriteLine("\n\nНатисніть будь-яку кнопку щоб продовжити...\n");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
--- C_Sharp_Dictionary/Program.cs
+++ C_Sharp_Dictionary/Program.cs
@@ -7,38 +7,6 @@ namespace C_Sharp_Dictionary_
 
     internal class Program
     {
-        class Menu
-        {
-
-            public void MenuStart()
-            {
-                Console.WriteLine("Меню:");
-                Console.WriteLine("" +
-                    "1. Обрати словник з яким ви хочете працювати:\n" +
-                    "2. Створити Новий словник");
-                Console.WriteLine("0. Вихід");
-            }
-            public void MenuMain(string dictName)
-            {
-                Console.WriteLine($"Ви працюєте із словником: \"{dictName}\"!\n\n" + $"Оберіть дію яку ви хочете виконати:\n" + 
-                    $"1. Додати нове слово\n" + 
-                    $"2. Замінити слово або його переклад\n" + 
-                    $"3. Шукати переклад слова \n" +
-                    $"4. Видалити слово або його переклад\n" +
-                    $"5. Показати весь словник\n" +
-                    $"6. Експортувати слово в окремий файл\n" +
-                    $"9. Обрати інший словник\n" +
-                    $"0. Вийти з програми");
-
-            }
-            public void Clear()
-            {
-                Console.WriteLine("\n\nНатисніть будь-яку кнопку щоб продовжити...\n");
-                Console.ReadKey();
-                Console.Clear();
-            }
-        }
-
         static void Main(string[] args)
         {
             
@@ -57,8 +25,6 @@ namespace C_Sharp_Dictionary_
             }
             catch (Exception ex) { Console.WriteLine(ex.Message); }
             
-
-
             while (!exit)
             {
                 
@@ -130,7 +96,6 @@ namespace C_Sharp_Dictionary_
 
                             }
                         }
-
                         
                             break;
                         case "2":
